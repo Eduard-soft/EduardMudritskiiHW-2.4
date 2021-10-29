70552d3179d2e4a0c8537e786939d24d15584a58
Eduard-soft <92379627+Eduard-soft@users.noreply.github.com>
2021-10-29 18:10:28 +0300

    Initial Commit

---
+++ EduardMudritskiiHW 2.4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EduardMudritskiiHW 2.4
+//
+//  Created by Эдуард on 29.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
